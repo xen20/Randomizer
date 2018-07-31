afcb530d7849cc009006aab84e2048be7f66a7cc
Konstantin <chernyjvoron@gmail.com>
2018-08-01 00:28:22 +0300

    implementing file copy checking using regex functionality

--- filecopyfunctions.h
+++ filecopyfunctions.h
@@ -2,6 +2,7 @@
 #define FILECOPYFUNCTIONS_H
 
 #include <QStringList>
+#include <QFileInfo>
 
 class fileCopyFunctions
 {
@@ -10,6 +11,19 @@ public:
     ~fileCopyFunctions();
     void fileCopy(QStringList sourceFiles, QString Destination);
     void folderCopy(QStringList sourceFiles, QString Destination);
-};
+    QString handleFilePathFunctions(QStringList sourceFiles, QString Destination);
+    bool checkIfDuplicateExistsAfterAppendingIndex();
 
+private:
+    /*in use by fileCopy function*/
+    int  fileCopyIndex;
+    int  fileIndex;
+    QStringList fileNameAfterSplit;
+    QStringList extractedDigits;
+    QString fileNameIfDuplicateExists;
+    QString destinationIfDuplicateExists;
+    bool checkIfDuplicateInDest;
+    bool doesDuplicateExistAfterAppendingIndex;
+    /*in use by filyCopy function*/
+};
 #endif // FILECOPYFUNCTIONS_H
